e8847d48bc82385a1152f6f166c0f7ac64e4d0e9
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2021-10-24 04:04:00 -0400

    revert to default

--- game-site/config.php
+++ game-site/config.php
@@ -8,7 +8,7 @@ $dbpass = 'test123';
 $dbhost = '127.0.0.1';
 
 $all_users_subbed = false;
-$server_id = "game1";
+$server_id = "beta";
 $pp_uri = '/web/ppemu.php'; # location of paypal emulator on game-servers
 # original is https://www.paypal.com/cgi-bin/webscr which obviously wont do
 # Dont set it to that though, as the paypalgateway.php is not implemented.
@@ -20,6 +20,6 @@ $EXHANGE_RATE = 100000; # How much 1 USD is worth as HI Money
 # Should be set to the same value on all game sites and the master site.
 # NOTE: if someone knows this secret they can login to ANYONES account
 # Ideally, this would be a random string of numbers, letters and symbols like 20 characters long T-T
-$hmac_secret = "c81f9522b9ecd84ad95af26d845a78d25208861555d4b18bf707eccf7b839d7c4cd635a38167552418e26838745336e7";
-$master_site = "//localhost:80";
+$hmac_secret = "!!NOTSET!!";
+$master_site = "//master.horse";
 ?>
