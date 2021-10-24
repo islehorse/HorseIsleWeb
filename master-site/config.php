04bef7e0089783f2acbc8116c9f8069b6e5c4bdf
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2021-10-24 04:06:36 -0400

    Update config.php

--- master-site/config.php
+++ master-site/config.php
@@ -10,5 +10,5 @@ $pp_uri = '[GAMESITE]/web/ppemu.php'; # location of paypal emulator on game-serv
 # Dont set it to that though, as the paypalgateway.php is not implemented.
 
 #should be same as all game-site's
-$hmac_secret = 'c81f9522b9ecd84ad95af26d845a78d25208861555d4b18bf707eccf7b839d7c4cd635a38167552418e26838745336e7';
+$hmac_secret = '!!NOTSET!!';
 ?>
