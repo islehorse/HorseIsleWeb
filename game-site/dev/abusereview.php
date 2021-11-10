34fde79a1a07a7d893b3ee8399a61c104c64acdf
Li <li@silica.codes>
2021-11-09 21:58:22 -0500

    update

---
+++ game-site/dev/abusereview.php
@@ -0,0 +1,7 @@
+
+<CENTER>
+<FONT FACE=ARIAL>
+<B>HorseIsle ABUSE REPORT Reviewer</B> [ <A HREF=?>RELIST ALL</A> ][ <A HREF=/account.php>EXIT</A> ]<BR>
+</CENTER>
+<TR><TD><B>No Abuse Reports</B><BR></TD></TR><HR>
+
---
+++ game-site/dev/moderatoractions.php
@@ -0,0 +1,7 @@
+
+<CENTER>
+<FONT FACE=ARIAL>
+<B>HorseIsle MODERATOR ACTIONS Reviewer</B> [ <A HREF=?>RELIST ALL</A> ][ <A HREF=/account.php>EXIT</A> ]<BR>
+</CENTER>
+<TR><TD><B>No New Moderator Actions</B><BR></TD></TR><HR>
+
